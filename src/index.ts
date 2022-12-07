e6431dfe941215e924c87b02c401e2865f28e7b6
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2022-12-07 17:46:13 +0000

    added helmet, cors and body-parser

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+
 import { PORT } from "./config";
 
 import routes from "./routes";
+import bodyParser from "body-parser";
 
 const app = express();
+
+app.use(helmet());
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(cors({ credentials: true, origin: true }));
 
 app.use(routes);
 
